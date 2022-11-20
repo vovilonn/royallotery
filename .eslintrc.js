9fb1b90ac5ab2f3a3e753c3920cae580bd0bd5e7
Sergey Beltsin <beltsins89@gmail.com>
2022-11-20 13:35:34 +0300

    add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "import/prefer-default-export": 0,
+    "no-param-reassign": 0,
+    "no-new": 0,
+    "import/extensions": 0,
+  },
+};
